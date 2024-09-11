60c70172487df931f299e73fdacea527544a6fd7
mohamedmeqlad99 <mohamedmeqlad9@gmail.com>
2024-09-11 18:53:03 +0300

    update data_visualization.py

---
+++ scripts/data_visualization.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import snowflake.connector
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+#  credentials
+conn = snowflake.connector.connect(
+    user='<your_snowflake_user>',
+    password='<your_snowflake_password>',
+    account='<your_snowflake_account>',
+    warehouse='<your_snowflake_warehouse>',
+    database='<your_snowflake_database>',
+    schema='<your_snowflake_schema>'
+)
+
+query = "SELECT * FROM customers"

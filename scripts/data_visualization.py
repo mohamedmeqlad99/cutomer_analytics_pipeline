354d6e26053eec69a23b4807de308498ab564e20
mohamedmeqlad99 <mohamedmeqlad9@gmail.com>
2024-09-12 20:18:01 +0300

    update data_visualization.py

--- scripts/data_visualization.py
+++ scripts/data_visualization.py
@@ -19,3 +19,14 @@ query = "SELECT * FROM customers"
 df = pd.read_sql(query, conn)
 
 conn.close()
+
+# -- plot date of birth --
+plt.figure(figsize=(10, 6))
+sns.histplot(df['date_of_birth'], bins=20, kde=False)
+plt.title('Customer Birth Year Distribution')
+plt.xlabel('Date of Birth')
+plt.ylabel('Frequency')
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
